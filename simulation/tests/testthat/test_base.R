71fb6fe0e9d7cb035512edbb4ad3e585e8b90f7b
klin1234@gmail.com <klin1234@gmail.com>
2017-11-26 09:31:16 -0500

    Added new tests, completed

--- simulation/tests/testthat/test_base.R
+++ simulation/tests/testthat/test_base.R
@@ -109,3 +109,51 @@ test_that("simulation_generator works with cores for matrices", {
     expect_true(all(res1[[i]] == res2[[i]]))
   }
 })
+
+test_that("simulation_generator works for errors", {
+  rule1 <- function(vec, ...){
+    if(vec[1] == 2) stop()
+    stats::rnorm(100, mean = vec[1])
+  }
+  criterion <- function(dat, vec, ...){
+    c(mean(dat), stats::sd(dat))
+  }
+  len <- 6
+  paramMat <- matrix(1:len, nrow = len)
+
+  trials <- 10
+  res1 <- simulation_generator(rule1, criterion, paramMat, trials = trials,
+                               verbose = F, as_list = F)
+
+  expect_true(is.list(res1))
+  expect_true(length(res1) == len)
+
+  rule2 <- function(vec, ...){
+    stats::rnorm(100, mean = vec[1])
+  }
+  res2 <- simulation_generator(rule2, criterion, paramMat, trials = trials,
+                               verbose = F, as_list = F)
+  for(i in c(1,3:len)){
+    expect_true(all(res1[[i]] == res2[[i]]))
+  }
+  expect_true(all(is.na(res1[[2]])))
+})
+
+test_that("simulation_generator can save", {
+  rule <- function(vec, ...){
+    stats::rnorm(100, mean = vec[1])
+  }
+  criterion <- function(dat, vec, ...){
+    mean(dat)
+  }
+  len <- 6
+  paramMat <- matrix(1:len, nrow = len)
+
+  trials <- 10
+  res <- simulation_generator(rule, criterion, paramMat, trials = trials,
+                              verbose = F, as_list = F, filepath = "tmp.RData")
+
+  expect_true(is.list(res))
+  file.remove("tmp.RData")
+})
+
